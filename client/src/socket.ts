0482910b718d477b07636b413dea442a5bb491d7
khuong291 <dkhuong291@gmail.com>
2019-08-09 15:01:18 +0700

    socket for prod

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,4 +1,6 @@
 import * as io from "socket.io-client";
-
-const socket = io(`localhost:${process.env.PORT || 5000}`);
+let socket = io(`localhost:5000`);
+if (process.env.PORT) {
+  socket = io("");
+}
 export default socket;
